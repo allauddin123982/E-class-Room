0638047218a4cf7840374cba688e0275cac3a17f
Allauddin <khanallauddin113@gmail.com>
2024-07-14 17:02:53 +0500

    token

--- src/setting.js
+++ src/setting.js
@@ -1,7 +1,7 @@
 import {createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "2fcc1159d1fa40f69b417b97a537b460"
-const token = "007eJxTYLjJziz/v+XB71chJZ0Sy5mSOyOCDW49PtKYZXvn1dOgCdwKDEZpycmGhqaWKYZpiSYGaWaWSSaG5kmW5ommxuZJJmYGtnbRaQ2BjAzbt7ezMjJAIIjPwuCbmJnHwAAAeBMfmw=="
+const token = "007eJxTYLj0/9G277/Pn5VkEYp9H37m2KL3YQxWtT6MLvXXFLjaBS4rMBilJScbGppaphimJZoYpJlZJpkYmidZmieaGpsnmZgZ2O6ZnNYQyMhwZ4E3AyMUgvgsDL6JmXkMDADA2iAt"
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
